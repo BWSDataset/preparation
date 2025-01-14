eeff3d1e7908a6cdae1fdbd03ddc8fb9c9ae09f4
Avisheak Das <avisheakdasshomrat510@gmail.com>
2025-01-14 22:01:25 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='preparation',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='A Bangla text preparation tool for summarization',
+    author='Avisheak Das',
+    author_email='avisheak.cse@std.cu.ac.bd',
+    url='https://github.com/avisheak/preparation',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
