3b79de52d4754766131f480e694e59e66ae455a6
Avisheak Das <avisheakdasshomrat510@gmail.com>
2025-01-14 22:29:49 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='preparation',
     version='0.1',
     packages=find_packages(),
-    install_requires=[],
-    description='A Bangla text preparation tool for summarization',
+    install_requires=[
+        "regex"
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Avisheak Das',
     author_email='avisheak.cse@std.cu.ac.bd',
     url='https://github.com/avisheak/preparation',
