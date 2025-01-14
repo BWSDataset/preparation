d7396f9316b7e989fc71824226194d15ddca4d32
Avisheak Das <avisheakdasshomrat510@gmail.com>
2025-01-14 23:18:58 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,16 @@ setuptools.setup(
     version="0.1",
     packages=['preparation'],
     install_requires=[
-        "regex"
+        "regex",
+        "pandas"
     ],
+    description="A custom tool for Bangla text preparation",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Avisheak Das',
     author_email='avisheak.cse@std.cu.ac.bd',
     url='https://github.com/avisheak/preparation',
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
     ],
 )
